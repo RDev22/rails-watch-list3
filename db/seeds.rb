1391d14fd038c00a01526ef898a4f698cff0e01e
Renan Dantas <rsdantas7@gmail.com>
2022-06-24 18:40:19 -0300

    alteraçoes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+
+url = "https://api.themoviedb.org/3/movie/top_rated?api_key=#{ENV['THE_MOVIE_DB_KEY']}&
+language=en-US&page=1"
+
+poster_base_ulr = 'https://image.tmdb.org/t/p/w500'
+movie_data = JSON.parse(URI.open(url).read)['results']
+
+movie_data.each do |movie|
+  Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: movie['poster_path'],
+    raiting:
+  )
+end
