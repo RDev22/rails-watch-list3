b152288cd0a14597e598d66d7721916fe5734435
Renan Dantas <rsdantas7@gmail.com>
2022-06-27 23:06:49 -0300

    add reviews to list

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'lists#index'
   resources :lists, only: %i[show new create], shallow: true do
     resources :bookmarks, only: %i[create destroy]
+    resources :reviews, only: %i[create]
   end
   #resources :bookmarks, only: %i[destroy]
 end
